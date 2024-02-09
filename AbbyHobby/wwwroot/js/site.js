772270cecb420562fac191887fe2d527778065b5
abbyharris <abbyharris@ABBYHARRISF75D>
2024-02-09 13:53:43 -0700

    Add project files.

---
+++ AbbyHobby/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$('#calculate-btn').click(function () {
+   var hours = parseFloat($('#hours').val());
+   var hourlyRate = parseFloat($('#hourly-rate').val().substring(1)); // Remove '$' sign
+   var total = hours * hourlyRate;
+
+   if (isNaN(hours) || hours <= 0) {
+       alert('Please enter a valid positive number for hours.');
+   } else {
+       $('#total').val('$' + total.toFixed(2));
+   }
+ });
